cd99e65612fdc0b51868fe691bba483c59dcd639
Stephan Gazarov <sgzrov@gmail.com>
2025-03-24 02:33:35 -0700

    Initial project setup: Firebase, HealthKit, app structure

---
+++ HealthPredictor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthPredictor
+//
+//  Created by Stephan  on 23.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Health Predictor App Initialized")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthPredictor/HealthPredictorApp.swift
@@ -0,0 +1,21 @@
+//
+//  HealthPredictorApp.swift
+//  HealthPredictor
+//
+//  Created by Stephan  on 23.03.2025.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct HealthPredictorApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
