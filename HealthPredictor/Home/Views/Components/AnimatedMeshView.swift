f375e98d99bf2f4e1700fa0440f186e2f89a58e2
Stephan Gazarov <sgzrov@gmail.com>
2025-04-30 22:44:12 -0700

    Refactor: expanded card view, ask ai button, card UI

---
+++ HealthPredictor/Home/Views/Components/AnimatedMeshView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedMeshView.swift
+//  HealthPredictor
+//
+//  Created by Stephan  on 29.04.2025.
+//
+
+import SwiftUI
+
+struct AnimatedMeshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedMeshView()
+}
