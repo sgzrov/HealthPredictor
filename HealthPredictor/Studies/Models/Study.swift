c40d3e1e7709976f806bdbee4e30b9ed2d1b691f
Stephan Gazarov <sgzrov@gmail.com>
2025-06-07 22:23:04 +0300

    feat: create model for study

--- HealthPredictor/Studies/Models/Study.swift
+++ HealthPredictor/Studies/Models/Study.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Study: Identifiable, Hashable, Codable {
+    let id: UUID
+    let title: String
+    let summary: String
+    let importDate: Date
+    let sourceURL: URL
+
+    init(id: UUID = UUID(), title: String, summary: String, sourceURL: URL) {
+        self.id = id
+        self.title = title
+        self.summary = summary
+        self.importDate = Date()
+        self.sourceURL = sourceURL
+    }
+
+    // Hashable conformance
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func == (lhs: Study, rhs: Study) -> Bool {
+        lhs.id == rhs.id
+    }
+}
