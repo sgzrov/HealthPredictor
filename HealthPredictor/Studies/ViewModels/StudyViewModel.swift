df6468009a070c8e25be02da206c4d443aa2eaef
Stephan Gazarov <sgzrov@gmail.com>
2025-06-16 17:43:54 +0300

    feat: add UI changes to study/category navigation

---
+++ HealthPredictor/Studies/ViewModels/StudyViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StudyViewModel.swift
+//  HealthPredictor
+//
+//  Created by Stephan  on 16.06.2025.
+//
+
+import Foundation
---
+++ HealthPredictor/Studies/Views/StudyCardView.swift
@@ -0,0 +1,18 @@
+//
+//  StudyCardView.swift
+//  HealthPredictor
+//
+//  Created by Stephan  on 16.06.2025.
+//
+
+import SwiftUI
+
+struct StudyCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudyCardView()
+}
