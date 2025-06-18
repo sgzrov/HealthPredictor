700038c0a5db0cd9461a7c373135909522730010
Stephan Gazarov <sgzrov@gmail.com>
2025-06-18 21:01:58 +0300

    feat: create model for chat message

---
+++ HealthPredictor/Chat/Models/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  HealthPredictor
+//
+//  Created by Stephan  on 18.06.2025.
+//
+
+import Foundation
--- HealthPredictor/Chatbot/ChatViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ChatViewModel.swift
-//  HealthPredictor
-//
-//  Created by Stephan  on 05.04.2025.
-
-//  Logic for user input, sending, appending messages
-
-import Foundation
