11247202331c0553269db86c73a4edde2785ba50
Stephan Gazarov <sgzrov@gmail.com>
2025-06-19 21:28:51 +0300

    feat: add trigger CSV file update on app start

--- HealthPredictor/HealthPredictorApp.swift
+++ HealthPredictor/HealthPredictorApp.swift
@@ -10,12 +10,15 @@ import Firebase
 
 @main
 struct HealthPredictorApp: App {
+
+    @Environment(\.scenePhase) private var scenePhase
+
     init() {
         HealthStoreService().requestAuthorization { success, error in
             if success {
                 print("✅ HealthKit authorized.")
             } else {
-                print("❌ HealthKit authorization failed: \(error?.localizedDescription ?? "Uknown error.")")
+                print("❌ HealthKit authorization failed: \(error?.localizedDescription ?? "Unknown error.")")
             }
         }
         FirebaseApp.configure()
@@ -24,6 +27,13 @@ struct HealthPredictorApp: App {
     var body: some Scene {
         WindowGroup {
             MainTabView()
+        }
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .active {
+                CSVManager.shared.generateCSV { fileURL in
+                    print("CSV generated at: \(fileURL?.absoluteString ?? "nil")")
+                }
+            }
         }
     }
 }
